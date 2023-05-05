83ed734e7d263fcb3cff8dbdc7b5b038e6588db6
Thaireis <tim.bieler@edu.tbz.ch>
2023-05-05 13:11:01 +0200

    Project begun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'sowasearch';
+  students: any;
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit() {
+    let response = this.http.get('http://localhost:8081/students');
+    response.subscribe((data) => (this.students = data));
+  }
 }
